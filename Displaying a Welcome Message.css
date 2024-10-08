334b40c2fe7f8a9ccef3c6ce58805f0b23f5e54e
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-08 22:51:23 +0800

    Create Displaying a Welcome Message.css
    Displaying a Welcome Message
    by CodeChum Admin
    
    Write a program that greets the user by displaying the message "Hello, World!" on the console.
    
    Sample Output 1
    Hello, World!

---
+++ Displaying a Welcome Message.css
@@ -0,0 +1,10 @@
+using System;
+
+class MainClass {
+
+    static public void Main()
+    {
+        // Hey there, start typing your C# code here...
+        Console.WriteLine("Hello, World!");
+    }
+}
