04d45133042e3215cdcfc1efe719eada119bd951
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-11 21:15:45 +0800

    Create Even Number Parade.cs
    Even Number Parade
    by CodeChum Admin
    
    Write a program that prints the first 5 even numbers, beginning from 2, with each number occupying its own line.
    
    Sample Output 1
    2
    4
    6
    8
    10

---
+++ C#/Even Number Parade.cs
@@ -0,0 +1,12 @@
+using System;
+namespace HelloWorld {
+    class program {
+        static void Main() {
+            Console.WriteLine("2");
+            Console.WriteLine("4");
+            Console.WriteLine("6");
+            Console.WriteLine("8");
+            Console.WriteLine("10");
+        }
+    }
+}
