0cb54c0477e84547158fa1d9bc807a180175076c
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-11 21:19:36 +0800

    Create CODE Symphony.cs
    CODE Symphony
    by CodeChum Admin
    
    Write a program that showcases a pattern of the word "CODE" using the letters C, O, D, and E.
    
    Sample Output 1
    CCCCC   OOOOO   DDDDD   EEEEE
    C            O       O    D         D  E
    C            O       O    D          D EEE
    C            O       O    D         D  E
    CCCCC   OOOOO   DDDDD   EEEEE

---
+++ C#/CODE Symphony.cs
@@ -0,0 +1,10 @@
+using System;
+class Program {
+    static void Main() {
+        Console.WriteLine("CCCCC   OOOOO   DDDDD   EEEEE");
+        Console.WriteLine("C       O   O   D   D   E    ");
+        Console.WriteLine("C       O   O   D   D   EEE  ");
+        Console.WriteLine("C       O   O   D   D   E    ");
+        Console.WriteLine("CCCCC   OOOOO   DDDDD   EEEEE");
+    }
+}
