413f6f618bc20fcfce6e5e6fd23935826c8d5387
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-11 21:16:37 +0800

    Create Starry Ascension.cs
    Starry Ascension
    by CodeChum Admin
    
    Write a program that generates a pattern of stars, ascending in quantity, as depicted below:
    
    *
    **
    ***
    ****
    *****
    
    Sample Output 1
    *
    **
    ***
    ****
    *****

---
+++ C#/Starry Ascension.cs
@@ -0,0 +1,13 @@
+using System;
+class Program {
+
+    static public void Main()
+    {
+        // Hey there, start typing your C# code here...
+        Console.WriteLine("*");
+        Console.WriteLine("**");
+        Console.WriteLine("***");
+        Console.WriteLine("****");
+        Console.WriteLine("*****");
+    }
+}
