415b745cd4ee7b2302ffd5e81caf16b20054277f
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-30 22:56:38 +0800

    Create Engaging_Character_Dialogues.cs
    Engaging Character Dialogues
    by CodeChum Admin
    
    Write a program that showcases dynamic dialogues between multiple characters, using escape sequences to accurately represent their conversations. Print out the dialogues with proper formatting, including appropriate spacing, quotation marks, and the correct usage of escape sequences for special characters. The following text is the dialogue to be printed:
    
    Character A:    "Hello there!"
    Character B:    'Hi! How are you?'
    Character A:    'I'm doing great.' Thanks for asking!
    Character C:    "Hey, what's going on?"
    Character B:    "We were just catching up."
    Character A:    "Yeah, it's been a while."
    Character C:    "Nice to see you both!"
    Character B:    "Likewise! How have you been?"
    Character C:    "I've been good. Thanks!"
    Character A:    "That's great to hear."
    
    Sample Output 1
    Character A:	"Hello there!"
    Character B:	'Hi! How are you?'
    Character A:	'I'm doing great.' Thanks for asking!
    Character C:	"Hey, what's going on?"
    Character B:	"We were just catching up."
    Character A:	"Yeah, it's been a while."
    Character C:	"Nice to see you both!"
    Character B:	"Likewise! How have you been?"
    Character C:	"I've been good. Thanks!"
    Character A:	"That's great to hear."

---
+++ C#/Basic Escape Sequences/Engaging_Character_Dialogues.cs
@@ -0,0 +1,16 @@
+using System;
+class Program{
+    static void Main() {
+        Console.WriteLine("Character A:\t\"Hello there!\"");
+        Console.WriteLine("Character B:\t'Hi! How are you?'");
+        Console.WriteLine("Character A:\t'I'm doing great.' Thanks for asking!");
+        Console.WriteLine("Character C:\t\"Hey, what's going on?\"");
+        Console.WriteLine("Character B:\t\"We were just catching up.\"");
+        Console.WriteLine("Character A:\t\"Yeah, it's been a while.\"");
+        Console.WriteLine("Character C:\t\"Nice to see you both!\"");
+        Console.WriteLine("Character B:\t\"Likewise! How have you been?\"");
+        Console.WriteLine("Character C:\t\"I've been good. Thanks!\"");
+        Console.WriteLine("Character A:\t\"That's great to hear.\"");
+        
+    }
+}
