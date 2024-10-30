df99eb499ea8c241f3a1b1c98e4caf2f21571668
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-30 18:41:18 +0800

    Create Book_Reservation_Details.cs
    Book Reservation Details
    by CodeChum Admin
    
    Create a program that displays the reservation details for a book titled "The Great Gatsby" by F. Scott Fitzgerald. The program should utilize appropriate placeholders for each detail. Output should look like:
    
    You have successfully reserved the book 'The Great Gatsby' by F. Scott Fitzgerald.
    Year of Publication: 1925
    Genre: Fiction
    Library: Central Library
    Member ID: 12345
    Return Date: June 30, 2023
    
    Sample Output 1
    You have successfully reserved the book 'The Great Gatsby' by F. Scott Fitzgerald.
    Year of Publication: 1925
    Genre: Fiction
    Library: Central Library
    Member ID: 12345
    Return Date: June 30, 2023

---
+++ C#/Placeholders/Book_Reservation_Details.cs
@@ -0,0 +1,19 @@
+using System;
+class Program {
+    static void Main() {
+        string title = "The Great Gatsby";
+        string author = "F. Scott Fitzgerald";
+        int year = 1925;
+        string genre = "Fiction";
+        string library = "Central Library";
+        int id = 12345;
+        string date = "June 30, 2023";
+        
+        Console.WriteLine("You have successfully reserved the book 'The Great Gatsby' by F. Scott Fitzgerald.");
+        Console.WriteLine("Year of Publication: {0}",year);
+        Console.WriteLine("Genre: {0}", genre);
+        Console.WriteLine("Library: {0}", library);
+        Console.WriteLine("Member ID: {0}", id);
+        Console.WriteLine("Return Date: {0}", date);
+    }
+}
