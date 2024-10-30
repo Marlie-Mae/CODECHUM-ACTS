de34f8a3f940731d20ed3a5c3bcee7254af89aed
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-30 22:54:28 +0800

    Create Mathematical_Equations.cs
    Mathematical Equations
    by CodeChum Admin
    
    Write a program that showcases five mathematical equations, each formatted with appropriate spacing and symbols. Employ horizontal tabs"\t" to align the components of each equation with a newline.
    
    Sample Output 1
    Equations:
    5	+	3	=	8
    12	-	7	=	5
    4	*	6	=	24
    20	/	5	=	4
    10	%	3	=	1

---
+++ C#/Basic Escape Sequences/Mathematical_Equations.cs
@@ -0,0 +1,11 @@
+using System;
+class Program {
+    static void Main() {
+        Console.WriteLine("Equations:");
+        Console.WriteLine("5\t+\t3\t=\t8");
+        Console.WriteLine("12\t-\t7\t=\t5");
+        Console.WriteLine("4\t*\t6\t=\t24");
+        Console.WriteLine("20\t/\t5\t=\t4");
+        Console.WriteLine("10\t%\t3\t=\t1");
+    }
+}
