63c151193e071ba17d34516dce5cc86c0b82472a
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-30 22:55:41 +0800

    Create Database_Record.cs
    Database Record
    by CodeChum Admin
    
    Create a program that prints the following text:
    
    Database Record
    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
    Name:        John Doe
    Email:        john.doe@example.com
    University:    ABC University
    
    Sample Output 1
    Database Record
    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
    Name:		John Doe
    Email:		john.doe@example.com
    University:	ABC University

---
+++ C#/Basic Escape Sequences/Database_Record.cs
@@ -0,0 +1,10 @@
+using System;
+class Program{
+    static void Main() {
+        Console.WriteLine("Database Record");
+        Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        Console.WriteLine("Name:\t\tJohn Doe");
+        Console.WriteLine("Email:\t\tjohn.doe@example.com");
+        Console.WriteLine("University:\tABC University");
+    }
+}
