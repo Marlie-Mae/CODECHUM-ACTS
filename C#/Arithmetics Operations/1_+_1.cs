1d5537f4871031cd6371f90fc945cd30d9b1e274
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-01 18:44:23 +0800

    Create 1_+_1.cs
    1 + 1
    by CodeChum Admin
    
    Write a program that adds two 1s and displays their output.
    
    Sample Output 1
    1 + 1 = 2

---
+++ C#/Arithmetics Operations/1_+_1.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+
+    static public void Main()
+    {
+        // Hey there, start typing your C# code here...
+        int a = 1;
+        int b = 1;
+        int c = a + b;
+        Console.WriteLine(a + " + " + b + " = " + c);
+    }
+}
