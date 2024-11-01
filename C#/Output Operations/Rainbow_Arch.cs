347b7349626008960ac21a0471b509b61484d12f
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-01 18:41:07 +0800

    Create Rainbow_Arch.cs
    Rainbow Arch
    by CodeChum Admin
    
    Write a program that brings the vibrant beauty of a rainbow to life by gracefully printing a colorful arch of hues. Copy the number of spaces before each color to mimic the arch of the rainbow as depicted below:
    
                Green
          Yellow     Brown
      Orange             Indigo
    Red                      Violet
    
    Sample Output 1
                Green
          Yellow     Brown
      Orange             Indigo
    Red                      Violet

---
+++ C#/Output Operations/Rainbow_Arch.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main(){
+        Console.WriteLine("            Green");
+        Console.WriteLine("      Yellow     Brown");
+        Console.WriteLine("  Orange             Indigo");
+        Console.WriteLine("Red                      Violet");
+    }
+}
