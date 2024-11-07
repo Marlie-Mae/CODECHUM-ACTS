6e46cba54809564aca73de0eb6462b2c2283d08e
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-07 18:23:09 +0800

    Create Two_Lover.cs
    Two Lovers
    by CodeChum Admin
    
    Write a program that asks for two string inputs, lover1, and lover2. After asking for input, print in the format: "{lover1} and {lover2} are in love."
    
    Sample Output 1
    Enter lover1: Jane
    Enter lover2: John
    Jane and John are in love.
    
    Sample Output 2
    Enter lover1: Pela
    Enter lover2: Lynx
    Pela and Lynx are in love.
    
    Sample Output 3
    Enter lover1: John
    Enter lover2: Doe
    John and Doe are in love.

---
+++ C#/String Handling/Two_Lover.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter lover1: ");
+        string lover1 = Console.ReadLine();
+        
+        Console.Write("Enter lover2: ");
+        string lover2 = Console.ReadLine();
+
+        Console.WriteLine($"{lover1} and {lover2} are in love.");
+    }
+}
