4b19f99576a55a36565ba1235170fcf085677ade
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-07 15:19:12 +0800

    Create Sentence_Length.cs
    Sentence Length
    by CodeChum Admin
    
    Write a program that calculates and prints the length of the given string in the code editor using the predefined string function.
    
    Sample Output 1
    Length of the string: 19

---
+++ C#/Pre-defined String Functions/Sentence_Length.cs
@@ -0,0 +1,9 @@
+using System;
+class Program {
+    static void Main(string[] args) {
+        string str = "Programming is Fun!";
+
+         // Start typing your code here...
+        Console.Write("Length of the string: " + str.Length);
+    }
+}
