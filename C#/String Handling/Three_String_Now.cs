aa3efd0cab4e78b39d55a9698ba0275c3c250c56
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-07 18:22:09 +0800

    Create Three_String_Now.cs
    Three Strings Now
    by CodeChum Admin
    
    Write a program that asks for three string inputs, first, second, and third. Print the following strings in the format: {first} - {second} - {third}
    
    Sample Output 1
    Enter the first string: Hello
    Enter the second string: World
    Enter the third string: Test
    Hello - World - Test
    
    Sample Output 2
    Enter the first string: First
    Enter the second string: Second
    Enter the third string: Third
    First - Second - Third
    
    Sample Output 3
    Enter the first string: Always
    Enter the second string: Been
    Enter the third string: Alive
    Always - Been - Alive

---
+++ C#/String Handling/Three_String_Now.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first string: ");
+        string first = Console.ReadLine();
+
+        Console.Write("Enter the second string: ");
+        string second = Console.ReadLine();
+
+        Console.Write("Enter the third string: ");
+        string third = Console.ReadLine();
+
+        Console.WriteLine($"{first} - {second} - {third}");
+    }
+}
