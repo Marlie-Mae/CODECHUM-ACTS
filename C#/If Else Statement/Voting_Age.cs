4a8ff9741cd1c4040bb8f9ec3ea6b30dbf63b068
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-20 23:48:51 +0800

    Create Voting_Age.cs

---
+++ C#/If Else Statement/Voting_Age.cs
@@ -0,0 +1,14 @@
+using System;
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+        
+        if (age >= 18) {
+            Console.WriteLine("You are eligible to vote.");
+        } else {
+            Console.WriteLine("You are not eligible to vote.");
+        }
+        
+    }
+}
